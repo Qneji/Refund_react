edc816ac4d046b15096dc8a13e209322aa801360
kenny <kennedy_ken99@hotmail.com>
2026-06-05 16:17:36 -0300

    capturando as informações do formulário

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react"
+
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
 export function SignIn(){
+  const[email, setEmail] = useState("")
+  const[password, setPassword] = useState("")
+  const[isLoading, setIsLoadind] = useState(false)
+
+  function onSubmit(e: React.SyntheticEvent){
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form  onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input 
         required 
         legend="E-mail" 
         type="email" 
         placeholder="Seu@email.com" 
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <Input 
@@ -16,9 +28,10 @@ export function SignIn(){
         legend="Senha" 
         type="password" 
         placeholder="123456" 
+        onChange={(e) => setPassword(e.target.value)}
       />
 
-      <Button>Entrar</Button>
+      <Button type="submit" isLoading={isLoading} >Entrar</Button>
     </form>
   )
 }
