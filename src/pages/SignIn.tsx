6bd0d8e9049e36259aa45a52e40a80a4514ad1ac
kenny <kennedy_ken99@hotmail.com>
2026-06-04 21:02:27 -0300

    Criando o formulário e utilizando o componente de Input

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,21 @@
+import { Input } from "../components/Input"
+
 export function SignIn(){
   return (
-    <h1>SignIn</h1>
+    <form className="w-full flex flex-col gap-4">
+      <Input 
+        required 
+        legend="E-mail" 
+        type="email" 
+        placeholder="Seu@email.com" 
+      />
+
+      <Input 
+        required 
+        legend="Senha" 
+        type="password" 
+        placeholder="123456" 
+      />
+    </form>
   )
 }
