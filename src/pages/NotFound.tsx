ed74373b6a7f4f87903de342de345325f924f0a4
kenny <kennedy_ken99@hotmail.com>
2026-06-07 23:31:18 -0300

    Criando página "Not Found"

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound(){
+  return (
+    <div>
+      <h1>Op's! Essa página não existe.😥</h1>
+      <a href="/">Voltar para o início</a>
+    </div>
+  )
+}
--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router";
 
 import { AuthLayout } from "../components/AuthLayout";
+
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
+import { NotFound } from "../pages/NotFound";
 
 
 export function AuthRoutes(){
@@ -12,6 +14,8 @@ export function AuthRoutes(){
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp  />} />
       </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
