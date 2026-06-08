1c6c33fe6ce80bb4f8191db0cc0a3e5d3562d544
kenny <kennedy_ken99@hotmail.com>
2026-06-07 23:41:21 -0300

    Estilizando a página "Not Found"

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,8 @@
 export function NotFound(){
   return (
-    <div>
-      <h1>Op's! Essa página não existe.😥</h1>
-      <a href="/">Voltar para o início</a>
+    <div className="w-screen h-screen bg-gradient-to-r from-gray-500 to-gray-200 flex flex-col justify-center items-center bg-img">
+      <h1 className="text-gray-100 font-semibold text-2xl mb-10">Op's! Essa página não existe.😥</h1>
+      <a href="/" className="font-semibold text-green-100 hover:text-green-200 transition ease-linear">Voltar para o início</a>
     </div>
   )
 }
