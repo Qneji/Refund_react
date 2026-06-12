dbcd41b6376171b088833ffc38e809c25fd6319e
kenny <kennedy_ken99@hotmail.com>
2026-06-12 14:06:06 -0300

    Estilizando o Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import logoutSvg from "../assets/logout.svg"
 
 export function Header(){
   return (
-    <header>
-      <img src={logoSvg} alt="Logo" />
+    <header className="w-full flex justify-between">
+      <img src={logoSvg} alt="Logo" className="my-8" />
 
-      <div>
-        <span>Olá, Kenny</span>
+      <div className="flex items-center gap-3">
+        <span className="text-sm font-semibold text-gray-200">Olá, Kenny</span>
 
-        <img src={logoutSvg} alt="Ícone de sair" />
+        <img src={logoutSvg} alt="Ícone de sair" className="my-8 cursor-pointer hover:opacity-75 transition ease-linear" />
       </div>
     </header>
   )
