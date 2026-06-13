b76c583d61d2dc2f5dce508552337f8cbb0e2aca
kenny <kennedy_ken99@hotmail.com>
2026-06-13 14:29:52 -0300

    Criando a pagina de confirmação e validando a entrada para confirmação de usuário

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
@@ -13,10 +14,13 @@ export function Refund() {
   const [isLoading, setIsLoading] = useState(false);
   const [filename, setFilename] = useState<File | null>(null);
 
+  const navigate = useNavigate()
+
   function onSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
 
     console.log(name, amount, category, filename);
+    navigate("/confirm", {state: { fromSubmit: true }})
   }
 
   return (
