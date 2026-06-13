e68b49f97a8b0ab70738f701a0a54ade160db048
kenny <kennedy_ken99@hotmail.com>
2026-06-13 12:21:49 -0300

    Criando o componente de upload

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories" 
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
+import { Upload } from "../components/Upload"
 
 export function Refund(){
   const [category, setCategory] = useState("")
@@ -31,6 +32,8 @@ export function Refund(){
 
         <Input legend="Valor" required />
       </div>
+
+      <Upload filename="123.png" />
     </form>
   )
 }
