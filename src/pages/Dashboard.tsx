c90a062525626159b71bb427e3bc50efc96b316f
kenny <kennedy_ken99@hotmail.com>
2026-06-14 14:28:19 -0300

    Criando as variantes do Botão

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
+
+import searchSvg from "../assets/search.svg"
+
 import { Input } from "../components/Input"
+import { Button } from "../components/Button"
 
 export function Dashboard() {
   const [name, setName] = useState("")
@@ -19,6 +23,10 @@ export function Dashboard() {
           placeholder="Pesquisar pelo nome"
           onChange={(e) => setName(e.target.value)}
         />
+
+        <Button type="submit" variant="icon">
+          <img src={searchSvg} alt="Ícone de pesquisar" className="w-5" />
+        </Button>
       </form>
     </div>
   )
