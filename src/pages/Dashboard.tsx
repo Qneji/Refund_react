2cccc72f0653af09a711841fb51757f383d9cfe9
kenny <kennedy_ken99@hotmail.com>
2026-06-18 06:04:03 -0300

    Customizando o Scroll

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -39,7 +39,7 @@ export function Dashboard() {
         </Button>
       </form>
 
-      <div>
+      <div className="mt-6 flex flex-col gap-4 max-h-85 overflow-y-scroll">
         <RefundItem data={REFUND_EXAMPLE} />
       </div>
     </div>
