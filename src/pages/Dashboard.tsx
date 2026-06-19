afd456bdd564d32e40b9648709d664dbcdd9d1c0
kenny <kennedy_ken99@hotmail.com>
2026-06-18 22:37:53 -0300

    Criando o componente de paginação

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,6 +6,7 @@ import { formatCurrency } from "../utils/formatCurrency"
 
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+import { Pagination } from "../components/Pagination" 
 import { RefundItem } from "../components/RefundItem"
 
 const REFUND_EXAMPLE = {
@@ -43,6 +44,8 @@ export function Dashboard() {
       <div className="mt-6 flex flex-col gap-4 max-h-85 overflow-y-scroll">
         <RefundItem data={REFUND_EXAMPLE} />
       </div>
+
+      <Pagination current={1} total={10} />
     </div>
   )
 }
