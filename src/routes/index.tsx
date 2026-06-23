1f99b2dd6917e689f98e20dce6d3e2ae934f0c2a
kenny <kennedy_ken99@hotmail.com>
2026-06-23 00:03:19 -0300

    Criando a Rota para cada usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,15 +7,33 @@ import { ManagerRoutes } from "./ManagerRoutes"
 import { EmployeeRoutes } from "./EmployeeRoutes"
 
 const isLoading = false
+//const session = undefined
+
+const session = {
+  user: {
+    role: "Employee",
+  },
+}
 
 export function Routes(){
+  function Route(){
+    switch (session?.user.role) {
+      case "Employee":
+        return <EmployeeRoutes />
+      case "manager":
+        return <ManagerRoutes />
+      default:
+        return <AuthRoutes />
+    }
+  }
+  
   if (isLoading) {
     return <Loading />
   }
 
   return(
     <BrowserRouter>
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   )
 }
